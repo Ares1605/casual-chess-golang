46e3eefb79276fa523f9e6e6ba8193f303a6f830
Ares Stavropoulos <aresstav04@gmail.com>
2024-10-16 21:14:55 -0500

    update rollup config

--- frontend/frontend/vite.config.ts
+++ frontend/frontend/vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: [
-        'wailsjs/go/models',
-        'wailsjs/runtime'
+        // '/wailsjs/go/models',
+        // '/wailsjs/runtime'
       ]
     }
   }
